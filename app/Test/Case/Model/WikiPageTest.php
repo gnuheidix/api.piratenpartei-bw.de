21545ae9a2a51b2485cd97d61cf5d4c19ccfa08a
Thomas Heidrich <heidi@miradlo.de>
2012-04-04 20:15:04 +0200

    Testbett initialisiert

---
+++ app/Test/Case/Model/WikiPageTest.php
@@ -0,0 +1,41 @@
+<?php
+App::uses('WikiPage', 'Model');
+
+class WikiPageTestCase extends CakeTestCase {
+    public $fixtures = array(
+        'app.wikiElement'
+        ,'app.wikiImage'
+        ,'app.wikiPage'
+    );
+    
+    public function setup(){
+        parent::setUp();
+        $this->WikiPage = ClassRegistry::init('WikiPage');
+    }
+    
+    /**
+     * Tests if a dataset can be retrieved from fixture settings.
+     */
+    public function testGetPageExisting(){
+        $result = $this->WikiPage->getPage("Kreisverband_Konstanz");
+        $this->assertEqual($result['WikiPage']['updatedat'], date('Y-m-d H:00:00', time()));
+        $this->assertEqual($result['WikiPage']['content'], '<div id="einmalig">Dies ist ein Test.</div>');
+    }
+    
+    /**
+     * Tests if a dataset can be retrieved from fixture settings and updated from the Wiki.
+     */
+    public function testGetPageExistingUpdated(){
+        $result = $this->WikiPage->getPage("BW:Kreisverband_Konstanz");
+        $this->assertNotEqual($result['WikiPage']['content'], '<div id="einmalig">Dies ist ein Test.</div>');
+    }
+    
+    /**
+     * Tests if a dataset can be retrieved from fixture settings and updated from the Wiki.
+     */
+    public function testGetPageExistingNotAvailable(){
+        $result = $this->WikiPage->getPage("nix:da");
+        $this->assertEqual($result, false);
+    }
+}
+?>
---
+++ app/Test/Fixture/WikiPageFixture.php
@@ -0,0 +1,28 @@
+<?php
+class WikiPageFixture extends CakeTestFixture {
+    
+    /**
+     * imports the table definition from regular database
+     */
+    public $import = 'WikiPage';
+    
+    /**
+     * some initial database records 
+     */
+    public $records;
+    
+    /**
+     * Fills up the database records beforehand.
+     */
+    public function __construct(){
+        
+        $this->records = array(
+            array('id' => 1, 'title' => 'Kreisverband_Konstanz', 'content' => '<div id="einmalig">Dies ist ein Test.</div>', 'requested' => date('Y-m-d H:00:00', time()), 'updatedat' => date('Y-m-d H:00:00', time()), 'created' => '2007-03-18 10:39:23')
+            ,array('id' => 2, 'title' => 'BW:Kreisverband_Konstanz', 'content' => '<div id="einmalig">Dies ist ein Test.</div>', 'requested' => '2007-03-18 10:39:23', 'updatedat' => '2007-03-18 10:39:23', 'created' => '2007-03-18 10:39:23')
+        );
+        
+        parent::__construct();
+    }
+ }
+ 
+?>
