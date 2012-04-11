fc054d8e6f58f64c8dd016c6c52c4f0ac571b1c9
Thomas Heidrich <heidi@miradlo.de>
2012-04-11 20:09:01 +0200

    InstallController gebaut, welcher von einem Dump heraus die DB aufsetzen kann, insofern es aktiviert und der dump bereitliegt.

---
+++ app/Controller/InstallController.php
@@ -0,0 +1,79 @@
+<?php //CPYRGHT
+/**
+ * api.piratenpartei-bw.de
+ * 
+ * @license MIT License (http://www.opensource.org/licenses/mit-license.php)
+ * @author  Thomas Heidrich
+ * @copyright Copyright (c) 2012 Thomas Heidrich and other authors
+ */
+?><?php
+/**
+ * Website content caching
+ *
+ * This file [reads, caches, delivers] content from other websites.
+ *
+ * PHP 5
+ *
+ * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
+ * Copyright 2005-2011, Cake Software Foundation, Inc. (http://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright 2005-2011, Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * @link          http://cakephp.org CakePHP(tm) Project
+ * @package       app.Controller
+ * @since         CakePHP(tm) v 0.2.9
+ * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
+
+App::uses('AppController', 'Controller');
+
+/**
+ * Does basic installation
+ */
+class InstallController extends AppController{
+    
+    /**
+     * Controller name
+     *
+     * @var string
+     */
+    public $name = 'Install';
+    
+    /**
+     * Helpers in use
+     * @var array
+     */
+    public $helpers = array();
+    
+    /**
+     * Models in use
+     * @var array
+     */
+    public $uses = array();
+    
+    /**
+     * Trigger installation
+     */
+    public function index(){
+        if(Configure::read('System.enableinstall')){
+            $filename = APP.'dump.sql';
+            ClassRegistry::init('ConnectionManager');
+            $db = ConnectionManager::getDataSource('default');
+            
+            $filecontent = file_get_contents($filename);
+            if($filecontent){
+                $data = $db->query($filecontent);
+                echo "gecallt ".$data;
+            }else{
+                echo "Datei ".$filename." nicht gefunden.";
+            }
+        }else{
+            echo "Installationsfunktion deaktiviert :-D";
+        }
+        exit;
+        
+    }
+}
+?>
